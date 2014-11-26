40093717ec277c367cbd825549864daddbf7d980
ntn11phm <ntn11phm@student.hig.se>
2014-11-26 14:48:24 +0100

    added GetEmailAddress Class

--- src/examProject/dao/GetEmailAddress.java
+++ src/examProject/dao/GetEmailAddress.java
@@ -1,5 +1,10 @@
 package examProject.dao;
 
 public class GetEmailAddress {
+	private String strCommand = "SELECT user_id FROM users WHERE username='";
+	
+	public String getUserEmailAddress(String username) {
+		
+	}
 
 }
