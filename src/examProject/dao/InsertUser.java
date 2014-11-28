16abdb9bfcfccc174303146300dbc76827d3bb7d
ntn11med <ntn11med@student.hig.se>
2014-11-28 10:32:42 +0100

    pwd convert char and AddUserLogic reworked

--- src/examProject/dao/InsertUser.java
+++ src/examProject/dao/InsertUser.java
@@ -1,11 +1,14 @@
 package examProject.dao;
 
+import examProject.logic.CharToStringConverter;
+
 public class InsertUser {
+	private CharToStringConverter conv = new CharToStringConverter();
 	private String sqlCommand = "INSERT INTO users (username, pwd, is_admin,) VALUES ('";
 
 	private String insertUserStrCommand(String username, char[] pwd, boolean is_admin) {
 		sqlCommand += username + "', '";
-		sqlCommand += pwd + "', '";
+		sqlCommand += conv.charToString(pwd)+ "', '";
 		sqlCommand += is_admin + "');";
 
 		return sqlCommand;
--- src/examProject/logic/AddUserLogic.java
+++ src/examProject/logic/AddUserLogic.java
@@ -6,43 +6,45 @@ import examProject.dao.DbInsert;
 import examProject.dao.DbSelect;
 
 public class AddUserLogic {
+	private CharToStringConverter conv = new CharToStringConverter();
 	private AddUser au;
 	private DbSelect dBs;
 	private DbInsert dBi;
-	
-	public AddUserLogic(AddUser au, DbSelect dBs, DbInsert dBi){
+
+	public AddUserLogic(AddUser au, DbSelect dBs, DbInsert dBi) {
 		this.au = au;
 		this.dBs = dBs;
 		this.dBi = dBi;
 	}
-	public boolean addUser(){
+
+	public boolean addUser() {
 		boolean result = false;
 		int userId = 0;
-		String sqlCommand = "SELECT username FROM users WHERE username = '" + au.getUserName() + "';";
+		String sqlCommand = "SELECT username FROM users WHERE username = '"
+				+ au.getUserName() + "';";
 		ResultSet rs = dBs.select(sqlCommand);
-		try{
-			if(!rs.next()){
+		try {
+			if (!rs.next()) {
 				sqlCommand = "INSERT INTO users (username, pwd, is_admin,) VALUES ('";
 				sqlCommand += au.userName + "', '";
-				sqlCommand += au.pwd + "', '";
+				sqlCommand += conv.charToString(au.pwd) + "', '";
 				sqlCommand += au.isAdmin + "');";
 				dBi.insert(sqlCommand);
-				sqlCommand =  "SELECT user_id FROM users WHERE username = '" + au.getUserName() + "';";
+				sqlCommand = "SELECT user_id FROM users WHERE username = '" + au.getUserName() + "';";
 				ResultSet rss = dBs.select(sqlCommand);
-				while(rss.next()){
+				while (rss.next()) {
 					userId = rss.getInt(0);
 				}
 				rss.close();
-				
-				sqlCommand = "INSERT INTO hosts (user_id, firstname, lastname) VALUES('";
+
+				sqlCommand = "INSERT INTO hosts (user_id, firstname, lastname) VALUES (user_id,'" + au.firstName + "', '" + au.lastName +"')";
 			}
-			
-		}catch(Exception e){
-			
+
+		} catch (Exception e) {
+
 		}
-		
+
 		return result;
 	}
-	
 
 }
