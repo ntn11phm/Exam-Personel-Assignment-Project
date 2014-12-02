ecadbf4eebe1eae28a2327fe2c023cbce4ac0bc8
ntn11med <ntn11med@student.hig.se>
2014-12-02 11:15:21 +0100

    Fipplar med Frames..

--- src/examProject/StartUp.java
+++ src/examProject/StartUp.java
@@ -1,9 +1,27 @@
 package examProject;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
 public class StartUp {
 
 	public static void main(String[] args) {
 		// Frame mainFrame = new Frame();
-	}
+		JFrame GuiMainFrame = new JFrame();
+		GuiMainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+		String titles[] = { "General", "Security", "Content", "Connection",
+				"Programs", "Advanced" };
+//		for (int i = 0, n = titles.length; i < n; i++) {
+//			add(tabbedPane, titles[i]);
+//		}
+
+		GuiMainFrame.add(tabbedPane, BorderLayout.CENTER);
+		GuiMainFrame.setSize(400, 150);
+		GuiMainFrame.setVisible(true);
+	}
 }
---
+++ src/examProject/ui/mainFrame/GuiMainFrame.java
@@ -0,0 +1,13 @@
+package examProject.ui.mainFrame;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import examProject.ui.addUser.AddUserGUI;
+
+public class GuiMainFrame {
+	JTabbedPane tabbedPane = new JTabbedPane();
+	AddUserGUI au = new AddUserGUI();
+	JPanel addUser = 
+
+}
