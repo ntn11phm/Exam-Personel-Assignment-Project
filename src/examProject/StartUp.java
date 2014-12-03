3e6f3c954fd076485f26cfd3a6fc97dafd5ea009
ntn11med <ntn11med@student.hig.se>
2014-12-03 11:25:43 +0100

    Refactoring and remove unused imports

--- src/examProject/StartUp.java
+++ src/examProject/StartUp.java
@@ -6,11 +6,9 @@ import examProject.ui.mainFrame.TabbedPane;
 public class StartUp {
 
 	public static void main(String[] args) {
-		// Frame mainFrame = new Frame();
 		TabbedPane tp = new TabbedPane();
 		tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tp.setSize(500, 500);
-		// tp.pack();
 		tp.setVisible(true);
 
 	}
--- src/examProject/ui/mainFrame/TabbedPane.java
+++ src/examProject/ui/mainFrame/TabbedPane.java
@@ -2,18 +2,14 @@ package examProject.ui.mainFrame;
 
 import java.awt.event.KeyEvent;
 
-import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.SwingUtilities;
-
 import examProject.ui.addUser.AddUserGUI;
 import examProject.ui.changePwd.ChangePwdPanel;
 import examProject.ui.forgotPwd.ForgotPwdPanel;
 
 public class TabbedPane extends JFrame {
+	private static final long serialVersionUID = -4337086054499823196L;
 
 	public TabbedPane() {
 		JTabbedPane jtp = new JTabbedPane();
@@ -28,24 +24,6 @@ public class TabbedPane extends JFrame {
 		jtp.setMnemonicAt(0, KeyEvent.VK_2);
 		jtp.addTab("Glömt Lösenord", new ForgotPwdPanel());
 		jtp.setMnemonicAt(0, KeyEvent.VK_3);
-
-	}
-
-	private static void createGui() {
-		TabbedPane tp = new TabbedPane();
-		tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		tp.setSize(500, 500);
-		// tp.pack();
-		tp.setVisible(true);
-	}
-
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				createGui();
-
-			}
-		});
 
 	}
 
