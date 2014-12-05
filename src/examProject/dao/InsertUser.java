f88524b7a52ec65177c754a9a888af2dc87c0fdf
ntn11med <ntn11med@student.hig.se>
2014-12-05 10:04:18 +0100

    Refactored

--- src/examProject/dao/InsertUser.java
+++ src/examProject/dao/InsertUser.java
@@ -6,8 +6,7 @@ public class InsertUser {
 	private PasswordHashing hash = new PasswordHashing();
 	private String sqlCommand = "INSERT INTO users (username, pwd, is_admin,) VALUES ('";
 
-	public String insertUserStrCommand(String username, char[] pwd,
-			boolean is_admin) {
+	public String insertUserStrCommand(String username, char[] pwd, boolean is_admin) {
 		sqlCommand += username + "', '";
 		sqlCommand += hash.createHashedPwd(pwd) + "', '";
 		sqlCommand += is_admin + "');";
