22e46a71e6cd878587bc25a73230f6af3ec9cf6b
ntn11phm <ntn11phm@student.hig.se>
2014-12-09 10:36:54 +0100

    updated PwdHashing

--- src/examProject/logic/PasswordHashing.java
+++ src/examProject/logic/PasswordHashing.java
@@ -2,17 +2,16 @@ package examProject.logic;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import examProject.dao.DbSelect;
+import examProject.dao.DbManipulator;
 
 public class PasswordHashing {
 	private CharToStringConverter conv = new CharToStringConverter();
-	private DbSelect dbSelect;
+	private DbManipulator dbm;
 	
 	public String createHashedPwd(char [] pwd) {
 		String result = "";
 		String sqlCommand = "SELECT crypt('" + conv.charToString(pwd) + "', gen_salt('bf', 8));";
-		ResultSet rs = dbSelect.select(sqlCommand);
+		ResultSet rs = dbm.select(sqlCommand);
 		try {
 			while (rs.next()) {
 				result = rs.getString(0);
