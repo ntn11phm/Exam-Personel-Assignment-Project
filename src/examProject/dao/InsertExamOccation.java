a406f7022a07aacf3e5d20613742683d2c3912ab
ntn11phm <ntn11phm@student.hig.se>
2014-12-09 11:43:02 +0100

    changed InsertExamOccation

--- src/examProject/dao/InsertExamOccation.java
+++ src/examProject/dao/InsertExamOccation.java
@@ -4,17 +4,19 @@ import examProject.transferObjects.ExamOccationTO;
 
 public class InsertExamOccation {
 	private String strCommand = "INSERT INTO examoccations (exam_date, exam_time, exam_location, booking_id, summary) VALUES ('";
-	
-	public String insertExamOccationCommand(String exam_date, String exam_time, String exam_location, String booking_id, String summary) {
-		strCommand += exam_date 	+ "', '";
-		strCommand += exam_time 	+ "', '";
-		strCommand += exam_location + "', '";
-		strCommand += booking_id 	+ "', '";
-		strCommand += summary		+ "');";
+
+	public String insertExamOccationCommand(String exam_date, String exam_time,	String exam_location, String booking_id, String summary) {
+			strCommand += exam_date 	+ "', '" 
+					+ exam_time 		+ "', '" 
+					+ exam_location		+ "', '" 
+					+ booking_id 		+ "', '" 
+					+ summary 			+ "');";
 		return strCommand;
 	}
-	
+
 	public String insertExamOccationCommand(ExamOccationTO currentTO) {
-		return insertExamOccationCommand(currentTO.getExamDate(), currentTO.getExamStartTime(), currentTO.getExamRoom(), currentTO.getBookingId(), currentTO.getSummary());
+		return insertExamOccationCommand(currentTO.getExamDate(),
+				currentTO.getExamStartTime(), currentTO.getExamRoom(),
+				currentTO.getBookingId(), currentTO.getSummary());
 	}
 }
