90ea102c1ee95acdd69e39efd2b11636bdd7e497
SilvanaYakob <hco11syb@student.hig.se>
2014-12-10 10:59:41 +0100

    Börja med klassen UppdateUserLogic

---
+++ src/examProject/logic/UppdateUserLogic.java
@@ -0,0 +1,23 @@
+package examProject.logic;
+import examProject.dao.DbManipulator;
+
+public class UppdateUserLogic {
+
+	private UppdateUser uppdateUser;
+	private DbManipulator dBm;
+	
+	public UppdateUserLogic(UppdateUser uppdateUser, DbManipulator dBm) {
+		this.uppdateUser = uppdateUser;
+		this.dBm = dBm;
+
+	}
+
+	public boolean uppdateUser() {
+		return false;
+	}
+
+}
+
+
+
+
