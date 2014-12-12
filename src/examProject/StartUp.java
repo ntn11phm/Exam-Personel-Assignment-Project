757ca3a1a1f4b925bd67696ce32fea2006c7c708
ntn11med <ntn11med@student.hig.se>
2014-12-12 16:09:27 +0100

    Added setupWindow

--- src/examProject/StartUp.java
+++ src/examProject/StartUp.java
@@ -1,16 +1,26 @@
 package examProject;
 
 import javax.swing.JFrame;
+
 import examProject.ui.mainFrame.TabbedPane;
+import examProject.ui.setUpDb.SetUpDbGui;
 
 public class StartUp {
 
 	public static void main(String[] args) {
 		TabbedPane tp = new TabbedPane();
+		JFrame frame = new JFrame();
+		SetUpDbGui setup = new SetUpDbGui();
 		if (tp.login()) {
+			tp.setTitle("Main Gui");
 			tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			tp.setSize(1000, 1000);
 			tp.setVisible(true);
+			frame.setTitle("Setup GUI");
+			frame.setSize(500, 500);
+			frame.add(setup);
+			frame.setVisible(true);
+			
 		}
 	}
 }
