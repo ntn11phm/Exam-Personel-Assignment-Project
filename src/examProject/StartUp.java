53282281df67f7a17239a9f59bf534f60d5e7349
ntn11med <ntn11med@student.hig.se>
2014-12-15 11:26:30 +0100

    refactored startup

--- src/examProject/StartUp.java
+++ src/examProject/StartUp.java
@@ -9,18 +9,11 @@ public class StartUp {
 
 	public static void main(String[] args) {
 		TabbedPane tp = new TabbedPane();
-		JFrame frame = new JFrame();
-		SetUpDbGui setup = new SetUpDbGui();
 		if (tp.login()) {
 			tp.setTitle("Main Gui");
 			tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			tp.setSize(1000, 1000);
 			tp.setVisible(true);
-			frame.setTitle("Setup GUI");
-			frame.setSize(500, 500);
-			frame.add(setup);
-			frame.setVisible(true);
-			
 		}
 	}
 }
