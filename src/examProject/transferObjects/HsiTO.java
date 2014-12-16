3b4aed718f4e066aeff0339958309b0825c55fe8
ntn11phm <ntn11phm@student.hig.se>
2014-12-16 12:32:56 +0100

    added answer to HsiTO

--- src/examProject/transferObjects/HsiTO.java
+++ src/examProject/transferObjects/HsiTO.java
@@ -3,10 +3,16 @@ package examProject.transferObjects;
 public class HsiTO {
 	private String date;
 	private String time;
+	private boolean answer;
 	
 	public HsiTO(String date, String time) {
+		this(date, time, false);
+	}
+	
+	public HsiTO(String date, String time, boolean answer) {
 		this.date = date;
 		this.time = time;
+		this.answer = answer;
 	}
 
 	public String getDate() {
@@ -15,6 +21,10 @@ public class HsiTO {
 
 	public String getTime() {
 		return time;
+	}
+	
+	public boolean getAnswer() {
+		return answer;
 	}
 
 	@Override
