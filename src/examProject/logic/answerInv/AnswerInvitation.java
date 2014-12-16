491b39215debbbc943b2969b32035f081e1e117d
ntn11phm <ntn11phm@student.hig.se>
2014-12-16 11:19:11 +0100

    updated AnswerInvitation

--- src/examProject/logic/answerInv/AnswerInvitation.java
+++ src/examProject/logic/answerInv/AnswerInvitation.java
@@ -1,5 +1,10 @@
 package examProject.logic.answerInv;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import examProject.dao.DbManipulator;
@@ -18,7 +23,19 @@ public class AnswerInvitation {
 	public List<HsiTO> getInvitations() {
 		List<HsiTO> result = null;
 		dbm.openDb();
-		String sqlCommand = "SELECT hsi_date, hsi_time FROM hosts_sessions_invitation WHERE host_id = (SELECT host_id FROM hosts WHERE user_id = " + currentUser.getUser_id() + ");";
+		String timeStamp = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+		String sqlCommand = "SELECT hsi_date, hsi_time FROM hosts_sessions_invitation WHERE host_id = (SELECT host_id FROM hosts WHERE user_id = "
+				+ currentUser.getUser_id() + ") AND hsi_date > '" + timeStamp + "' ORDER BY hsi_date, hsi_time;";
+		ResultSet rs = dbm.select(sqlCommand);
+		try {
+			while (rs.next()) {
+				if (result == null)
+					result = new ArrayList<HsiTO>();
+				result.add(new HsiTO(rs.getString("hsi_date"), rs.getString("hsi_time")));
+			}
+			rs.close();
+		} catch (SQLException e) {}
+		dbm.closeDb();
 		return result;
 	}
 }
