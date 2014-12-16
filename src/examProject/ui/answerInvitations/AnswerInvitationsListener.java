af8624cb2265e143118d7890986d7dcccb5e1cfc
ntn11phm <ntn11phm@student.hig.se>
2014-12-16 14:00:09 +0100

    updated AnswerInvitationsListener

--- src/examProject/ui/answerInvitations/AnswerInvitationsListener.java
+++ src/examProject/ui/answerInvitations/AnswerInvitationsListener.java
@@ -1,8 +1,14 @@
 package examProject.ui.answerInvitations;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+
 import examProject.logic.BackendFacade;
 import examProject.transferObjects.HsiTO;
 
@@ -10,6 +16,7 @@ public class AnswerInvitationsListener {
 	private AnswerInvitationsPanel answerPanel;
 	private BackendFacade backendFacade;
 	private List<HsiTO> hsiList;
+	private JPanel panel;
 	
 	public AnswerInvitationsListener(AnswerInvitationsPanel answerPanel, BackendFacade backendFacade) {
 		this.answerPanel = answerPanel;
@@ -24,11 +31,22 @@ public class AnswerInvitationsListener {
 	private void loadInv() {
 		hsiList = backendFacade.getHsiList();
 		DisplayInvitationsPanel dip = new DisplayInvitationsPanel(hsiList);
-		answerPanel.getScollPane().setViewportView(dip.getPanel());
+		this.panel = dip.getPanel();
+		answerPanel.getScollPane().setViewportView(panel);
 		answerPanel.repaint();
 	}
 	
 	private void commitAnswers() {
-		
+		List<HsiTO> answerList = new ArrayList<HsiTO>();
+		for (Component c: panel.getComponents()) {
+			boolean answer;
+			if (c instanceof JCheckBox) {
+				String componentName = c.getName();
+				String[] parts = componentName.split("_");
+				answer = ((JCheckBox) c).isSelected();
+				if (parts.length==2)
+					answerList.add(new HsiTO(parts[1], parts[0], answer));
+			}
+		}
 	}
 }
