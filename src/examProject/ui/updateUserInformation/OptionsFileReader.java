d9fcd66dc674811bd71ed883e5282dd4f2a47e3c
SilvanaYakob <hco11syb@student.hig.se>
2014-12-17 10:07:14 +0100

    Options.txt Reader and save It to OptionsFileReader

--- src/examProject/ui/updateUserInformation/OptionsFileReader.java
+++ src/examProject/ui/updateUserInformation/OptionsFileReader.java
@@ -1,58 +1,42 @@
 package examProject.ui.updateUserInformation;
-	import java.io.BufferedWriter;
-	import java.io.FileNotFoundException;
-	import java.io.FileReader;
-	import java.io.FileWriter;
-	import java.util.Scanner;
-	import java.io.File;
-	import java.io.FileReader;
-	import java.io.IOException;
-	import examProject.transferObjects.ExamOccationTO;
 
-	public class OptionsFileReader {
-		private String dataFile = "";
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
-		
-		public void readOptionFile() throws NullPointerException {
-		
+public class OptionsFileReader {
 
-			 try {
-		            Scanner scan = new Scanner(System.in);
-		              File file = new File("D:/Gitup/Exam-Personel-Assignment-Project/Options.txt");
-		              scan = new Scanner(file);
+	@SuppressWarnings("resource")
+	public void readOptionFile() throws NullPointerException {
 
-		            while (scan.hasNextLine()) {
-		                String line = scan.nextLine(); 
-		                System.out.println("linenumber " + line);
-		                try {
-		                	 
-		        			String content = "";
-		         
-		        			File file1 = new File("D:/filename.txt");
-		         
-		        			if (!file1.exists()) {
-		        				file1.createNewFile();
-		        			}
-		         
-		        			FileWriter fw = new FileWriter(file1.getAbsoluteFile());
-		        			BufferedWriter bw = new BufferedWriter(fw);
-		        			//bw.write(content);
-		        			bw.write(line);
+		try {
+			Scanner scan = new Scanner(System.in);
+			File openedFile = new File("Options.txt");
+			scan = new Scanner(openedFile);
 
-		        			bw.close();
-		         
-		        			System.out.println("Done");
-		         
-		        		} catch (IOException e) {
-		        			e.printStackTrace();
-		        		}
-		        }
-		            scan.close();
+			try {
+				File saveFileTo = new File("OptionsFileReader.txt");
 
-		        } catch (Exception ex) {
-		            ex.printStackTrace();
-		        }
-		    }		
+				if (!saveFileTo.exists()) {
+					saveFileTo.createNewFile();
+				}
+				FileWriter fileWriter = new FileWriter(saveFileTo.getAbsoluteFile());
+				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+				while (scan.hasNextLine()) {
+					String line = scan.nextLine();
+					bufferedWriter.write(line);
+					bufferedWriter.newLine();
+				}
+				bufferedWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			scan.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
-	
-	
+}
