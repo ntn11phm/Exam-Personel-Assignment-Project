766c5bb436397d0f17d6b1e99bc709361bed9510
ntn11phm <ntn11phm@student.hig.se>
2014-12-18 09:40:25 +0100

    Added HostTO Class

---
+++ src/examProject/transferObjects/HostTO.java
@@ -0,0 +1,25 @@
+package examProject.transferObjects;
+
+public class HostTO {
+	private int host_id;
+	private String firstName;
+	private String lastName;
+	
+	public HostTO(int host_id, String firstName, String lastName) {
+		this.host_id = host_id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getHost_id() {
+		return host_id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+}
