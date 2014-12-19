7ec33d7eaec86d95f98cc910a6425919f6d33762
ntn11phm <ntn11phm@student.hig.se>
2014-12-19 09:45:00 +0100

    updated populate sessions

--- src/examProject/logic/populateSessions/PopulateSessions.java
+++ src/examProject/logic/populateSessions/PopulateSessions.java
@@ -2,6 +2,7 @@ package examProject.logic.populateSessions;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import examProject.dao.DbManipulator;
 
@@ -10,6 +11,20 @@ public class PopulateSessions {
 	
 	public PopulateSessions(DbManipulator dbm) {
 		this.dbm = dbm;
+	}
+	
+	public List<String> loadAvailableHosts(String date, String time) {
+		dbm.openDb();
+		String sqlCommand = "SELECT host_id FROM hosts_sessions_invitation WHERE hsi_date = '" + date 
+				+ "' AND hsi_time = '" + time + "' AND answer_date = '1970-01-01'" + ";";
+		ResultSet rs = dbm.select(sqlCommand);
+		try {
+			while (rs.next()){
+				
+			}
+		} catch (SQLException e) {}
+		dbm.closeDb();
+		return null;
 	}
 	
 	public boolean checkHostSessionAvailabillity(String date, int host_id) {
