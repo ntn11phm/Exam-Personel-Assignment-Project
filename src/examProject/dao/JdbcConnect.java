f1102d216db28cbe55d5511d8099b8c67a6218aa
SilvanaYakob <hco11syb@student.hig.se>
2014-12-19 11:23:41 +0100

    Password

--- src/examProject/dao/JdbcConnect.java
+++ src/examProject/dao/JdbcConnect.java
@@ -38,7 +38,7 @@ public class JdbcConnect {
 			Class.forName("org.postgresql.Driver");
 			c = DriverManager.getConnection(
 					"jdbc:" + dbConnectionTo.getDatabaseDriver() + "://" + dbConnectionTo.getDatabasePath() + ":" + dbConnectionTo.getDatabasePort() + "/" + dbConnectionTo.getDatabaseName(), 
-					"postgres", "Destroyer");
+					"postgres", "silvana");
 			
 			System.out.println("Opened database successfully");
 		} 
