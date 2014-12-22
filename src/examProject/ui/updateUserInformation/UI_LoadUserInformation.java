2a0eb0f17f495c6385935e117334c67fba722593
ntn11phm <ntn11phm@student.hig.se>
2014-12-22 12:17:08 +0100

    removed import

--- src/examProject/ui/updateUserInformation/UI_LoadUserInformation.java
+++ src/examProject/ui/updateUserInformation/UI_LoadUserInformation.java
@@ -1,31 +1,16 @@
 package examProject.ui.updateUserInformation;
-import java.awt.BorderLayout;
-import java.awt.Checkbox;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
-
-import examProject.dao.GetUserID;
 import examProject.logic.BackendFacade;
-import examProject.logic.OptionsFileReader;
-import examProject.transferObjects.AddUser;
-import examProject.transferObjects.LoggedInUserTO;
 import examProject.transferObjects.UpdateUserTransfere;
 
 @SuppressWarnings("serial")
