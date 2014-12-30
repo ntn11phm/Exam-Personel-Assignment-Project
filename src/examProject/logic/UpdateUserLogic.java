f654d9a7fe771875291a7b352841e58ac9d4fe0d
ntn11phm <ntn11phm@student.hig.se>
2014-12-30 13:26:34 +0100

    added getLogginUserData() to UpdateUserLogic

--- src/examProject/logic/UpdateUserLogic.java
+++ src/examProject/logic/UpdateUserLogic.java
@@ -1,6 +1,7 @@
 package examProject.logic;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import examProject.dao.DbManipulator;
 import examProject.dao.UpdateUsersInformation;
 import examProject.transferObjects.LoggedInUserTO;
@@ -11,23 +12,35 @@ public class UpdateUserLogic {
 	private DbManipulator dBm;
 	private LoggedInUserTO currentUser;
 
-	public UpdateUserLogic(LoggedInUserTO currentUser,
-			UpdateUserTransfere uppdateUser, DbManipulator dBm) {
+	public UpdateUserLogic(LoggedInUserTO currentUser, UpdateUserTransfere uppdateUser, DbManipulator dBm) {
 		this.uppdateUser = uppdateUser;
 		this.dBm = dBm;
-
+	}
+	
+	public UpdateUserTransfere getLogginUserData() {
+		UpdateUserTransfere storedUserdata = null;
+		dBm.openDb();
+		String sqlCommand = "SELECT first_name, last_name, civicnr, address, zipcode, city, phone_nr, mobile_phone, email, is_active FROM hosts WHERE host_id =" + currentUser.getHost_id() + ";";
+		ResultSet rs = dBm.select(sqlCommand);
+		try {
+			while (rs.next())
+				storedUserdata = new UpdateUserTransfere(rs.getString("first_name"), rs.getString("last_name"),
+						rs.getString("email"), rs.getString("email"), rs.getString("city"),
+						rs.getString("address"), rs.getString("mobile_phone"), rs.getString("phone_nr"),
+						rs.getString("zipcode"), rs.getString("civicnr"), rs.getBoolean("is_active"), currentUser.isIs_admin());
+		} catch (SQLException e) {}
+		dBm.closeDb();
+		return storedUserdata;
 	}
 
 	public boolean uppdateUser() {
 		dBm.openDb();
 		boolean result = false;
-		String sqlCommand = "SELECT user_id FROM hosts WHERE first_name='" + uppdateUser.getFirstName() + "';";
+		String sqlCommand = "SELECT user_id FROM hosts WHERE host_id='" + currentUser.getHost_id() + "';";
 		ResultSet rs = dBm.select(sqlCommand);
-
 		try {
 			if (!rs.next()) {
-				UpdateUsersInformation updateUser = new UpdateUsersInformation(
-						uppdateUser, dBm);
+				UpdateUsersInformation updateUser = new UpdateUsersInformation(uppdateUser, dBm);
 				dBm.insert(updateUser.updateUserStrCommand());
 
 				sqlCommand = "UPDATE hosts SET first_name='"
