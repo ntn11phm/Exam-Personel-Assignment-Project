5467252475142b3e8b1c0b128b118ad772dc3892
ntn11phm <ntn11phm@student.hig.se>
2015-01-02 10:19:11 +0100

    undone invoke later

--- src/examProject/StartUp.java
+++ src/examProject/StartUp.java
@@ -1,27 +1,16 @@
 package examProject;
 
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
 import examProject.ui.mainFrame.TabbedPane;
 
 public class StartUp {
-	private static TabbedPane tp;
 
 	public static void main(String[] args) {
-		
-		  SwingUtilities.invokeLater(new Runnable()
-		    {
-		        @Override
-		        public void run()
-		        {
-		        	tp = new TabbedPane();
-		    		while (!tp.isUserLoggedIn()) {
-		    			
-		    		}
-		        }
-		    });
-		
+		TabbedPane tp = new TabbedPane();
+		while (!tp.isUserLoggedIn()) {
+			
+		}
 		if (tp.isUserLoggedIn()) {
 			tp.setTitle("Main Gui");
 			tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
