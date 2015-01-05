c3deed2220a109cb3dd1768b1854322322fa0158
ntn11phm <ntn11phm@student.hig.se>
2015-01-05 13:34:07 +0100

    refactoring

--- src/examProject/dao/DbManipulator.java
+++ src/examProject/dao/DbManipulator.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import examProject.transferObjects.DBConnectionTO;
 
 public class DbManipulator {
@@ -24,7 +23,6 @@ public class DbManipulator {
 			state = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 			rs = state.executeQuery(selectCommand);
 			result = rs;
-			System.out.println("Operation done successfully");
 		} catch (Exception e) {}
 		return result;
 	}
@@ -36,7 +34,6 @@ public class DbManipulator {
 			state = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 			state.executeUpdate(insertCommand);
 			result = true;
-			System.out.println("Records created successfully");
 		} catch (Exception e) {System.out.println(e);}
 		return result;
 	}
@@ -48,7 +45,6 @@ public class DbManipulator {
 			state = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 			state.executeUpdate(updateCommand);
 			result = true;
-			System.out.println("Update successful");
 		} catch (Exception e) {System.out.println(e);}
 
 		return result;
@@ -61,7 +57,6 @@ public class DbManipulator {
 			state = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 			state.executeUpdate(deleteCommand);
 			result = true;
-			System.out.println("Delete successfully done!");
 		} catch (Exception e) {System.out.println(e);}
 		return result;
 	}
--- src/examProject/dao/JdbcConnect.java
+++ src/examProject/dao/JdbcConnect.java
@@ -33,22 +33,11 @@ public class JdbcConnect {
 		try {
 			Class.forName("org.postgresql.Driver");
 			c = DriverManager.getConnection(
-					"jdbc:" + dbConnectionTo.getDatabaseDriver() + "://" + dbConnectionTo.getDatabasePath() + ":" + dbConnectionTo.getDatabasePort() + "/" + dbConnectionTo.getDatabaseName(), 
-					username, pwd);
-			
-			System.out.println("Opened database successfully");
-		} 
-		 
-		// original kod nedan bara try-sats		
-		/*
-		 * try { Class.forName("org.postgresql.Driver");
-		 * Class.forName(dbConnectionTo.getDatabaseDriver()); c =
-		 * DriverManager.getConnection(
-		 * "jdbc:postgresql://localhost:5432/Tentamensprojekt", "postgres",
-		 * "Destroyer"); System.out.println("Opened database successfully"); }
-		 */ 
-		
-		catch (Exception e) {}
+					"jdbc:" + dbConnectionTo.getDatabaseDriver() 
+					+ "://" + dbConnectionTo.getDatabasePath() 
+					+ ":" + dbConnectionTo.getDatabasePort() 
+					+ "/" + dbConnectionTo.getDatabaseName(), username, pwd);
+		} catch (Exception e) {}
 		
 		return c;
 	}
@@ -62,10 +51,8 @@ public class JdbcConnect {
 			try {
 				if (con != null)
 					con.close();
-				System.out.println("Closed database successfully");
 			} catch (SQLException se) {	}
 		}
-		
 		return true;
 	}
 }
