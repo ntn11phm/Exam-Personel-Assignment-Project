a8cafd01b26a00bdb2d55ca53d1fdfa41e2d46d9
ntn11phm <ntn11phm@student.hig.se>
2015-01-08 19:20:36 +0100

    added javadoc to mail_interface

--- src/examProject/logic/mail/Mail_Interface.java
+++ src/examProject/logic/mail/Mail_Interface.java
@@ -1,10 +1,32 @@
 package examProject.logic.mail;
-
+/**
+ * Email interface for the Exam-Personel-Assignment-Project.
+ * @author Per Hedblom
+ */
 import java.util.List;
 
 public interface Mail_Interface {
-
+	/**
+	 * Send-function.
+	 * @param to {@link String} a valid email-address.
+	 * @param from {@link String} a valid email-address.
+	 * @param message {@link String} body-text.
+	 * @param subject {@link String} 
+	 */
 	public void send(String to, String from, String message, String subject);
+	/**
+	 * Send-function.
+	 * @param to {@link String} a valid email-address.
+	 * @param message {@link String} body-text.
+	 * @param subject {@link String}
+	 */
 	public void send(String to, String message, String subject);
+	/**
+	 * Send-function.
+	 * @param to {@link List} of {@link String} a valid email-addresses.
+	 * @param from {@link String} a valid email-address.
+	 * @param message {@link String} body-text.
+	 * @param subject {@link String}
+	 */
 	void send(List<String> to, String from, String message, String subject);
 }
