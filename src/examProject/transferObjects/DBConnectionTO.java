a58a8cae03873410bd178475e5a97c72e7c2c832
ntn11phm <ntn11phm@student.hig.se>
2015-01-09 20:28:58 +0100

    javadoc + refactoring

--- src/examProject/transferObjects/DBConnectionTO.java
+++ src/examProject/transferObjects/DBConnectionTO.java
@@ -1,68 +1,82 @@
 package examProject.transferObjects;
-
+/**
+ * Transferobject containing data needed to create the connection-string
+ * @author Per Hedblom
+ * @author Silvana Yacob
+ */
 public class DBConnectionTO {
-	//private String usernames;
 	private String databaseDriver;
 	private String databasePath;
 	private String databasePort;
-	private String mailServerPath;
 	private String databaseName;
 	private boolean firstTimeLaunch;
-		
-	public DBConnectionTO(){
-	}
-	
+	/**
+	 * Getter for the database name.
+	 * @return {@link String}
+	 */
 	public String getDatabaseName() {
 		return databaseName; 
 	}
-
+	/**
+	 * Setter for the database name.
+	 * @param databaseName {@link String}
+	 */
 	public void setDatabaseName(String databaseName) {
 		this.databaseName = databaseName;
 	}
-
-//	public String getUsernames() {
-//		return usernames;
-//	}
-
+	/**
+	 * Getter for the database driver.
+	 * @return {@link String}
+	 */
 	public String getDatabaseDriver() {
 		return databaseDriver;
 	}
-
+	/**
+	 * Getter for the database URL.
+	 * @return {@link String}
+	 */
 	public String getDatabasePath() {
 		return databasePath;
 	}
-
+	/**
+	 * Getter for the port to the database.
+	 * @return {@link String}
+	 */
 	public String getDatabasePort() {
 		return databasePort;
 	}
-
-	public String getMailServerPath() {
-		return mailServerPath;
-	}
-
+	/**
+	 * Getter for the flag checking if the setup has been done before.
+	 * @return {@link boolean}
+	 */
 	public boolean isFirstTimeLaunch() {
 		return firstTimeLaunch;
 	}
-//	public void setUsernames(String usernames) {
-//		this.usernames = usernames;
-//	}
-
+	/**
+	 * Setter for the database driver.
+	 * @param databaseDriver {@link String}
+	 */
 	public void setDatabaseDriver(String databaseDriver) {
 		this.databaseDriver = databaseDriver;
 	}
-
+	/**
+	 * Setter for the URL to the database.
+	 * @param databasePath {@link String}
+	 */
 	public void setDatabasePath(String databasePath) {
 		this.databasePath = databasePath;
 	}
-
+	/**
+	 * Setter of the port to the database.
+	 * @param databasePort {@link String}
+	 */
 	public void setDatabasePort(String databasePort) {
 		this.databasePort = databasePort;
 	}
-
-	public void setMailServerPath(String mailServerPath) {
-		this.mailServerPath = mailServerPath;
-	}
-
+	/**
+	 * Setter for the flag  checking if the setup has been done before.
+	 * @param firstTimeLaunch {@link boolean}
+	 */
 	public void setFirstTimeLaunch(boolean firstTimeLaunch) {
 		this.firstTimeLaunch = firstTimeLaunch;
 	}
