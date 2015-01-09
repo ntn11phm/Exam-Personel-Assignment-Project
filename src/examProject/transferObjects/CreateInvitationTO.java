498c5c34b71f6f0b47d9ce24fa2559d774db0c16
ntn11phm <ntn11phm@student.hig.se>
2015-01-09 20:18:36 +0100

    added javadoc
    toString() seems strange in this one

--- src/examProject/transferObjects/CreateInvitationTO.java
+++ src/examProject/transferObjects/CreateInvitationTO.java
@@ -1,28 +1,43 @@
 package examProject.transferObjects;
-
+/**
+ * Transferobject containing two dates and a toString()
+ * @author Magnus Eklund
+ */
 import java.util.List;
 
 public class CreateInvitationTO {
-
 	private String fromDate;
 	private String toDate;
-
+	/**
+	 * Constructor.
+	 * @param fromDate {@link String} use dateformat: yyyy-MM-dd
+	 * @param toDate {@link String} use dateformat: yyyy-MM-dd
+	 */
 	public CreateInvitationTO(String fromDate, String toDate) {
 		this.fromDate = fromDate;
 		this.toDate = toDate;
 	}
-
+	/**
+	 * Getter for the fromdate.
+	 * @return {@link String}
+	 */
 	public String getFromDate() {
 		return fromDate;
 	}
-
+	/**
+	 * Getter for the todate.
+	 * @return {@link String}
+	 */
 	public String getToDate() {
 		return toDate;
 	}
-
+	/**
+	 *  Here for unclear reasons :P
+	 * @param sessionList
+	 * @return {@link String}
+	 */
 	public String toString(List<CreateInvitationTO> sessionList) {
 		return "" + fromDate + "   " + toDate;
-
 	}
 
 }
